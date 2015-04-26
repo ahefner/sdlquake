907bc8a9c96ca46d60db351c152f831d9da04b0c
Andy Hefner <ahefner@gmail.com>
2015-04-26 14:16:56 -0400

    Fix some warnings

--- quakedef.h
+++ quakedef.h
@@ -44,6 +44,7 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 #include <stdarg.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdint.h>
 #include <setjmp.h>
 
 #if defined(_WIN32) && !defined(WINDED)
